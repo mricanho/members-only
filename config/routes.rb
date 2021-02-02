5628a7eb24dd62ed0109295d8f1f97912379e5cc
Miguel Ricaño <mricanho@gmail.com>
2021-02-01 18:41:17 -0600

    Specify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts
+  resources :posts, skip: [:destroy, :edit, :show, :update]
   root "posts#index"
 end
